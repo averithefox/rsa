201b3f53e8fe8e309288639dbc9a75d723126310
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-02-01 19:24:37 +1100

    oops

--- src/main/java/com/ricedotwho/rsa/mixins/MixinPlayer.java
+++ src/main/java/com/ricedotwho/rsa/mixins/MixinPlayer.java
@@ -1,9 +1,9 @@
 package com.ricedotwho.rsa.mixins;
 
+import com.ricedotwho.rsa.module.impl.dungeon.Dungeonbreaker;
 import com.ricedotwho.rsm.RSM;
 import com.ricedotwho.rsm.component.impl.location.Island;
 import com.ricedotwho.rsm.component.impl.location.Loc;
-import com.ricedotwho.rsm.module.impl.dungeon.DungeonbreakerHelper;
 import com.ricedotwho.rsm.utils.ItemUtils;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -21,8 +21,8 @@ public abstract class MixinPlayer {
 
     @Inject(method = "getDestroySpeed", at = @At("RETURN"), cancellable = true)
     private void modifyBreakSpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
-        if (Loc.getArea().is(Island.Dungeon) && "DUNGEONBREAKER".equals(ItemUtils.getID(this.getInventory().getSelectedItem())) && RSM.getModule(DungeonbreakerHelper.class).isEnabled()) {
-            if (DungeonbreakerHelper.canInstantMine(state)) {
+        if (Loc.getArea().is(Island.Dungeon) && "DUNGEONBREAKER".equals(ItemUtils.getID(this.getInventory().getSelectedItem())) && RSM.getModule(Dungeonbreaker.class).isEnabled()) {
+            if (Dungeonbreaker.canInstantMine(state)) {
                 cir.setReturnValue(1500f);
             } else {
                 cir.setReturnValue(0f);
