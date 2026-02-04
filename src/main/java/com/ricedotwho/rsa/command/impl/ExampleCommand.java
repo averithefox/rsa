f8ff4f294bd9994330938ed51be4d2272f148305
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-02-04 18:45:01 +1100

    Update ExampleCommand.java

--- src/main/java/com/ricedotwho/rsa/command/impl/ExampleCommand.java
+++ src/main/java/com/ricedotwho/rsa/command/impl/ExampleCommand.java
@@ -2,13 +2,9 @@ package com.ricedotwho.rsa.command.impl;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.ricedotwho.rsm.RSM;
 import com.ricedotwho.rsm.command.Command;
 import com.ricedotwho.rsm.command.api.CommandInfo;
-import com.ricedotwho.rsm.module.Module;
 import com.ricedotwho.rsm.utils.ChatUtils;
-import com.ricedotwho.rsm.utils.ConfigUtils;
-import com.ricedotwho.rsm.utils.FileUtils;
 import net.minecraft.client.multiplayer.ClientSuggestionProvider;
 
 @CommandInfo(name = "example", description = "An example command")
@@ -18,12 +14,17 @@ public class ExampleCommand extends Command {
     public LiteralArgumentBuilder<ClientSuggestionProvider> build() {
         return literal(name())
                 .executes(ctx -> {
-                    ChatUtils.chat("Hi!");
+                    ChatUtils.chat("...");
                     return 1;
                 })
-                .then(literal("hi")
+                .then(argument("input", StringArgumentType.word())
                         .executes(ctx -> {
-                            ChatUtils.chat("...");
+                            String input = StringArgumentType.getString(ctx, "input");
+                            if (input.equalsIgnoreCase("hi")) {
+                                ChatUtils.chat("Hi!");
+                            } else {
+                                ChatUtils.chat("...");
+                            }
                             return 1;
                         })
                 );
