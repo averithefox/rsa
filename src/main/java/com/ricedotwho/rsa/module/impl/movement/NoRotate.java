5a45cf8c96f5da738c171ac12548a0b555acb032
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-02-05 22:06:18 +1100

    norotate use tp items

--- src/main/java/com/ricedotwho/rsa/module/impl/movement/NoRotate.java
+++ src/main/java/com/ricedotwho/rsa/module/impl/movement/NoRotate.java
@@ -2,32 +2,107 @@ package com.ricedotwho.rsa.module.impl.movement;
 
 
 import com.ricedotwho.rsa.mixins.LocalPlayerAccessor;
+import com.ricedotwho.rsa.module.impl.dungeon.DungeonBreaker;
+import com.ricedotwho.rsm.component.impl.location.Island;
+import com.ricedotwho.rsm.component.impl.location.Location;
+import com.ricedotwho.rsm.component.impl.map.handler.Dungeon;
+import com.ricedotwho.rsm.event.api.SubscribeEvent;
+import com.ricedotwho.rsm.event.impl.client.PacketEvent;
+import com.ricedotwho.rsm.event.impl.game.DungeonEvent;
+import com.ricedotwho.rsm.event.impl.world.WorldEvent;
 import com.ricedotwho.rsm.module.Module;
 import com.ricedotwho.rsm.module.api.Category;
 import com.ricedotwho.rsm.module.api.ModuleInfo;
+import com.ricedotwho.rsm.ui.clickgui.settings.impl.BooleanSetting;
+import com.ricedotwho.rsm.utils.ChatUtils;
+import com.ricedotwho.rsm.utils.ItemUtils;
+import com.ricedotwho.rsm.utils.Utils;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
-import net.minecraft.network.protocol.game.ServerboundAcceptTeleportationPacket;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.world.entity.PositionMoveRotation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
-@ModuleInfo(aliases = "No Rotate", id = "No Rotate", category = Category.MOVEMENT)
+@ModuleInfo(aliases = "No Rotate", id = "NoRotate", category = Category.MOVEMENT)
 public class NoRotate extends Module {
+
+    private final BooleanSetting teleportItem = new BooleanSetting("Teleport Items", true);
+    private final BooleanSetting outbounds = new BooleanSetting("Outbounds", false);
+    private final BooleanSetting alwaysNoRotate = new BooleanSetting("Always No Rotate", false);
+
+    private static final List<Block> ignored = Arrays.asList(
+            Blocks.HOPPER,
+            Blocks.ANVIL,
+            Blocks.DAMAGED_ANVIL,
+            Blocks.CHIPPED_ANVIL,
+            Blocks.CHEST, // copper chest ?
+            Blocks.TRAPPED_CHEST,
+            Blocks.ENDER_CHEST,
+            Blocks.DROPPER,
+            Blocks.DISPENSER,
+            Blocks.LEVER
+    );
+
+    private boolean shouldNoRotate = false;
 
     public NoRotate() {
         this.registerProperty(
-
+                teleportItem,
+                outbounds,
+                alwaysNoRotate
         );
     }
 
+    // maybe this should have a timeout
+    @SubscribeEvent
+    public void onUseItem(PacketEvent.Send event) {
+        if (!this.teleportItem.getValue() || Dungeon.isInBoss()) return;
+        if (event.getPacket() instanceof ServerboundUseItemPacket packet) {
+            ItemStack stack = mc.player.getItemBySlot(packet.getHand().asEquipmentSlot());
+            if (isHoldingTpItem(stack)) {
+                shouldNoRotate = true;
+            }
+        } else if (event.getPacket() instanceof ServerboundUseItemOnPacket packet) {
+            ItemStack stack = mc.player.getItemBySlot(packet.getHand().asEquipmentSlot());
+            Block block =  mc.level.getBlockState(packet.getHitResult().getBlockPos()).getBlock();
+            if (!ignored.contains(block) && isHoldingTpItem(stack)) {
+                shouldNoRotate = true;
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onEnterBoss(DungeonEvent.EnterBoss event) {
+        reset();
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        reset();
+    }
+
+    private boolean shouldNoRotate() {
+        return this.alwaysNoRotate.getValue()
+                || (this.teleportItem.getValue() && shouldNoRotate
+                || this.outbounds.getValue() && !Dungeon.isStarted() && Location.getArea().is(Island.Dungeon)
+        );
+    }
+
+    // Is there a reason to not use PacketEvent?
     public void onHandleMovePlayer(ClientboundPlayerPositionPacket packet, Connection connection, CallbackInfo ci) {
-        if (!this.isEnabled()) return;
+        if (!this.isEnabled() || !shouldNoRotate()) return;
+        shouldNoRotate = false;
         // Thanks noob for the code
 
         LocalPlayer player = Minecraft.getInstance().player;
@@ -54,17 +129,13 @@ public class NoRotate extends Module {
     }
 
     @Override
-    public void onEnable() {
-
-    }
-
-    @Override
-    public void onDisable() {
-
-    }
-
-    @Override
     public void reset() {
+        shouldNoRotate = false;
+    }
 
+    private boolean isHoldingTpItem(ItemStack item) {
+        String sbId = ItemUtils.getID(item);
+        if (Utils.equalsOneOf(sbId, "ASPECT_OF_THE_END", "ASPECT_OF_THE_VOID", "ETHERWARP_CONDUIT", "ASPECT_OF_THE_LEECH_1", "ASPECT_OF_THE_LEECH_2", "ASPECT_OF_THE_LEECH_3")) return true;
+        return Utils.equalsOneOf(sbId, "NECRON_BLADE", "SCYLLA", "HYPERION", "VALKYRIE", "ASTRAEA") && ItemUtils.getCustomData(item).getListOrEmpty("ability_scroll").size() == 3;
     }
 }
