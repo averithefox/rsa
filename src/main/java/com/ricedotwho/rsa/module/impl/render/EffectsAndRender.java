6553ed2ceaa9b8bf2faa68a46fb5af202809df6c
NewUser1121 <randomalt153@gmail.com>
2026-02-06 03:07:02 -0600

    got rid of repetitive comments in EffectsAndRender, also added a bunch of annoying effects like nausea and such. also added a fps counter.

--- src/main/java/com/ricedotwho/rsa/module/impl/render/EffectsAndRender.java
+++ src/main/java/com/ricedotwho/rsa/module/impl/render/EffectsAndRender.java
@@ -1,15 +1,26 @@
 package com.ricedotwho.rsa.module.impl.render;
 
 import com.ricedotwho.rsm.RSM;
+import com.ricedotwho.rsm.data.Colour;
+import com.ricedotwho.rsm.event.api.SubscribeEvent;
+import com.ricedotwho.rsm.event.impl.game.ClientTickEvent;
+import com.ricedotwho.rsm.event.impl.render.Render2DEvent;
 import com.ricedotwho.rsm.module.Module;
 import com.ricedotwho.rsm.module.api.Category;
 import com.ricedotwho.rsm.module.api.ModuleInfo;
 import com.ricedotwho.rsm.ui.clickgui.settings.impl.BooleanSetting;
+import com.ricedotwho.rsm.ui.clickgui.settings.impl.DragSetting;
+import com.ricedotwho.rsm.utils.render.render2d.NVGUtils;
 import lombok.Getter;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.effect.MobEffects;
+import org.joml.Vector2d;
 
 @Getter
 @Environment(EnvType.CLIENT)
@@ -19,17 +30,71 @@ public class EffectsAndRender extends Module {
     private final BooleanSetting Fires = new BooleanSetting("Fires", false, () -> true);
     private final BooleanSetting EtherWarp = new BooleanSetting("EtherWarp", false, () -> true);
     private final BooleanSetting SMOKE = new BooleanSetting("SMOKE", false, () -> true);
+    private final BooleanSetting Nausea = new BooleanSetting("Nausea", false, () -> true);
+    private final BooleanSetting Blindness = new BooleanSetting("Blindness", false, () -> true);
+    private final BooleanSetting Slowness = new BooleanSetting("Slowness", false, () -> true);
+    private final BooleanSetting Haste = new BooleanSetting("Haste", false, () -> true);
+    private final BooleanSetting Darkness = new BooleanSetting("Darkness", false, () -> true);
+    private final BooleanSetting Mining_Fatigue = new BooleanSetting("Mining Fatigue", false, () -> true);
+    private final BooleanSetting Speedness = new BooleanSetting("Speedness", false, () -> true);
+    private final BooleanSetting FpsToggled= new BooleanSetting("Fps display", false, () -> true);
+    private final DragSetting Fps = new DragSetting("Fps display", new Vector2d(50, 50), new Vector2d(50, 50));
 
     public EffectsAndRender() {
         this.registerProperty(
                 Explosions,
                 Fires,
                 EtherWarp,
-                SMOKE
+                SMOKE,
+                FpsToggled,
+                Fps,
+                Nausea,
+                Blindness,
+                Slowness,
+                Haste,
+                Speedness,
+                Darkness,
+                Mining_Fatigue
         );
     }
 
+    @SubscribeEvent
+    public void onClientTick(ClientTickEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null) return;
+
+        if(Nausea.getValue()) {
+            mc.player.removeEffect(MobEffects.NAUSEA);
+        }
+
+        if(Blindness.getValue()) {
+            mc.player.removeEffect(MobEffects.BLINDNESS);
+        }
+
+        if(Slowness.getValue()) {
+            mc.player.removeEffect(MobEffects.SLOWNESS);
+        }
+
+        if(Haste.getValue()) {
+            mc.player.removeEffect(MobEffects.HASTE);
+        }
+
+        if(Speedness.getValue()) {
+            mc.player.removeEffect(MobEffects.SPEED);
+        }
+
+        if(Darkness.getValue()) {
+            mc.player.removeEffect(MobEffects.DARKNESS);
+        }
+
+        if(Mining_Fatigue.getValue()) {
+            mc.player.removeEffect(MobEffects.MINING_FATIGUE);
+        }
+    }
+
     public static void init() {
+        Minecraft mc = Minecraft.getInstance();
+        ClientLevel level = mc.level;
         //FUCK YOU EXPLOSIONS
         ParticleFactoryRegistry.getInstance().register(
                 ParticleTypes.EXPLOSION,
@@ -133,6 +198,7 @@ public class EffectsAndRender extends Module {
                             RSM.getModule(EffectsAndRender.class).SMOKE.getValue() ? null : originalFactory.createParticle(simpleParticleType, clientLevel, d, e, f, g, h, i, randomSource);
                 }
         );
+
     }
 
     @Override
@@ -145,5 +211,19 @@ public class EffectsAndRender extends Module {
 
     @Override
     public void reset() {
+    }
+
+    @SubscribeEvent
+    public void onRender2D(Render2DEvent event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        ClientLevel level = Minecraft.getInstance().level;
+
+        if (player == null || level == null) return;
+        int fps = Minecraft.getInstance().getFps();
+        String fpsString = "Fps: " + fps;
+
+        if (FpsToggled.getValue()) {
+            this.Fps.renderScaled(event.getGfx(), () -> NVGUtils.drawText(fpsString, 0, 0, 50f, Colour.blue, NVGUtils.NUNITO), 60, 30);
+        }
     }
 }
