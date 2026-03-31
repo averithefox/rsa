443cb451afcd02bb2e2d65fa7d0d83c71a30c21f
rice.who <100787688+ricedotwho@users.noreply.github.com>
2026-03-31 21:45:59 +1100

    schizo maybe invwalk limbo fix?

--- src/main/java/com/ricedotwho/rsa/module/impl/dungeon/boss/p3/terminals/auto/AutoTerms.java
+++ src/main/java/com/ricedotwho/rsa/module/impl/dungeon/boss/p3/terminals/auto/AutoTerms.java
@@ -5,7 +5,10 @@ import com.google.common.primitives.Shorts;
 import com.google.common.primitives.SignedBytes;
 import com.ricedotwho.rsa.RSA;
 import com.ricedotwho.rsa.event.impl.RawTickEvent;
+import com.ricedotwho.rsa.module.impl.dungeon.boss.Blink;
 import com.ricedotwho.rsa.module.impl.dungeon.boss.p3.terminals.auto.terminals.*;
+import com.ricedotwho.rsa.module.impl.dungeon.boss.p3.terminals.auto.terminals.Colors;
+import com.ricedotwho.rsa.module.impl.dungeon.boss.p3.terminals.auto.terminals.Melody;
 import com.ricedotwho.rsm.RSM;
 import com.ricedotwho.rsm.component.impl.Terminals;
 import com.ricedotwho.rsm.component.impl.location.Island;
@@ -21,23 +24,28 @@ import com.ricedotwho.rsm.module.Module;
 import com.ricedotwho.rsm.module.api.Category;
 import com.ricedotwho.rsm.module.api.ModuleInfo;
 import com.ricedotwho.rsm.ui.clickgui.settings.group.GroupSetting;
-import com.ricedotwho.rsm.ui.clickgui.settings.impl.BooleanSetting;
-import com.ricedotwho.rsm.ui.clickgui.settings.impl.MultiBoolSetting;
-import com.ricedotwho.rsm.ui.clickgui.settings.impl.NumberSetting;
+import com.ricedotwho.rsm.ui.clickgui.settings.impl.*;
+import com.ricedotwho.rsm.utils.ChatUtils;
 import com.ricedotwho.rsm.utils.DungeonUtils;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.HashedStack;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.common.ClientboundPingPacket;
 import net.minecraft.network.protocol.game.*;
 import net.minecraft.world.entity.player.Input;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
@@ -276,16 +284,21 @@ public class AutoTerms extends Module {
             this.clickedWindow = false;
             this.invWalkGroup.getValue().getTerminalRenderer().newWindow(terminalContainer);
 
-            if (invWalkGroup.getValue().isEnabled()) event.setCancelled(true);
+            if (invWalkGroup.getValue().isEnabled()) {
+                event.setCancelled(true);
+                if (invWalkGroup.getValue().getInvwalkMaybeFix().getValue()) setContainerMenu(packet.getType(), mc, packet.getContainerId(), packet.getTitle());
+            }
             return;
         }
+
+        boolean cancel = !invWalkGroup.getValue().getInvwalkMaybeFix().getValue();
 
         if (isInTerm() && event.getPacket() instanceof ClientboundContainerSetSlotPacket packet) {
             if (packet.getContainerId() == 0 || packet.getContainerId() != this.terminalContainer.containerId) return;
             terminalContainer.setItem(packet.getSlot(), packet.getStateId(), packet.getItem());
             terminal.loadSlot(packet);
 
-            if (invWalkGroup.getValue().isEnabled()) event.setCancelled(true);
+            if (invWalkGroup.getValue().isEnabled() && cancel) event.setCancelled(true);
             return;
         }
 
@@ -303,12 +316,12 @@ public class AutoTerms extends Module {
         }
 
         if (isInTerm() && event.getPacket() instanceof ClientboundSetCursorItemPacket packet) {
-            if (invWalkGroup.getValue().isEnabled()) event.setCancelled(true);
+            if (invWalkGroup.getValue().isEnabled() && cancel) event.setCancelled(true);
             return;
         }
 
         if (isInTerm() && event.getPacket() instanceof ClientboundContainerSetContentPacket packet) {
-            if (packet.containerId() != 0 && invWalkGroup.getValue().isEnabled()) event.setCancelled(true);
+            if (packet.containerId() != 0 && invWalkGroup.getValue().isEnabled() && cancel) event.setCancelled(true);
             return;
         }
 
@@ -318,13 +331,23 @@ public class AutoTerms extends Module {
         }
 
         if (isInTerm() && event.getPacket() instanceof ClientboundContainerSetDataPacket packet) {
-            if (packet.getContainerId() != 0 && invWalkGroup.getValue().isEnabled()) event.setCancelled(true);
+            if (packet.getContainerId() != 0 && invWalkGroup.getValue().isEnabled() && cancel) event.setCancelled(true);
             return;
         }
 
         if (isInTerm() && event.getPacket() instanceof ClientboundMerchantOffersPacket) {
             reset();
             return;
+        }
+    }
+
+    public static <T extends AbstractContainerMenu> void setContainerMenu(MenuType<T> menuType, Minecraft minecraft, int i, Component component) {
+        MenuScreens.ScreenConstructor<T, ?> screenConstructor = MenuScreens.getConstructor(menuType);
+        if (screenConstructor == null) {
+            RSA.getLogger().warn("Failed to create screen for menu type: {}", BuiltInRegistries.MENU.getKey(menuType));
+        } else {
+            Screen screen = screenConstructor.create(menuType.create(i, minecraft.player.getInventory()), minecraft.player.getInventory(), component);
+            mc.player.containerMenu = ((MenuAccess) screen).getMenu();
         }
     }
 
